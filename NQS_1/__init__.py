d2acb65f33a7165f7298e80ebe8f64363d923fbd
Serranofa <facu64_17@hotmail.com>
2021-10-22 17:38:09 -0300

    transform

---
+++ NQS_1/__init__.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the
+#   NQS Project (https://github.com/DisenioSoftware2021/Neural-Quantum-State).
+# Copyright (c) 2021, Facundo Serrano
+# License: MIT
+#   Full Text: https://github.com/milicolazo/Pyedra/blob/master/LICENSE         <----------- OJO CON ESTO, CAMBIAR
+
+# =============================================================================
+# DOCS
+# =============================================================================
+
+"""
+NQS.
+
+Represents a quantum state using the restricted Boltzmann machine.         <----------- OJO CON ESTO, CAMBIAR
+"""
+
+# =============================================================================
+# META
+# =============================================================================
+
+__version__ = "0.1.0"
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+from .core import *  # noqa
+
+from .condprobability1 import *  # noqa
+
+from .gradient import *  # noqa
+
+from .hamiltonian import *  # noqa
+
+from .mcmethod import *  # noqa
+
+from .nqs import *  # noqa
+
+from .quantumodel1 import *  # noqa
+
+from .trainer import *  # noqa
