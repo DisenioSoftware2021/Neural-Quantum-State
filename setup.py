8a133526dd3eb299ab0e5a04c047e039223806a7
agoscono <agostinaaichino@gmail.com>
2021-11-02 10:57:47 -0300

    update

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     author_email="facundo.serrano@mi.unc.edu.ar",
     url="https://github.com/DisenioSoftware2021/Neural-Quantum-State",
     packages=[
-        "NQS_1",
+        "NQS",
     ],
     license="The MIT License",# <-------------------- CONSULTAR
     install_requires=REQUIREMENTS,
